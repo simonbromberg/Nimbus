3eea8c8e4f02ddfc49b9b35e0ffe298bbd40931d
simonbromberg <bshimmy@gmail.com>
2022-03-17 00:24:27 -0400

    Initial Commit

---
+++ Nimbus/NimbusApp.swift
@@ -0,0 +1,17 @@
+//
+//  NimbusApp.swift
+//  Nimbus
+//
+//  Created by Simon Bromberg on 2022-03-17.
+//
+
+import SwiftUI
+
+@main
+struct NimbusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
